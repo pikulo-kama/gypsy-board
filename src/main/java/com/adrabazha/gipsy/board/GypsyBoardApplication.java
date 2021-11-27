094a1c7a86c81541611f116f21aa5ecd8315782c
Artur Drabazha <adrabazha@agileassets.com>
2021-11-27 02:09:02 +0200

    Setup project

---
+++ src/main/java/com/adrabazha/gipsy/board/GypsyBoardApplication.java
@@ -0,0 +1,13 @@
+package com.adrabazha.gipsy.board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GypsyBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GypsyBoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adrabazha/gipsy/board/GipsyBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adrabazha.gipsy.board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GipsyBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
